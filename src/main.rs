765e21e729bcd305e54211d8f207401936b83a2f
Rodion Borovyk <rodion.borovyk@gmail.com>
2024-08-31 20:21:33 +0200

    files

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,34 @@
 mod threadpool;
 
 use std::{
-    io::{BufRead, BufReader, Error, Write},
+    env,
+    io::{BufRead, BufReader, Error, Read, Write},
     net::{TcpListener, TcpStream},
 };
 
 use threadpool::ThreadPool;
 
+const OK: &[u8] = b"HTTP/1.1 200 OK";
+const CRLF: &[u8] = b"\r\n";
+const NOT_FOUND: &[u8] = b"HTTP/1.1 404 Not Found\r\n\r\n";
+
 fn main() {
     println!("Logs from your program will appear here!");
+    let mut directory: Option<String> = None;
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 3 && args.get(1).unwrap() == "--directory" {
+        directory = Some(args[2].to_owned());
+    }
 
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     let mut thread_pool = ThreadPool::new();
 
     for incoming in listener.incoming() {
+        let dir = directory.clone();
         thread_pool.execute(|| match incoming {
             Ok(stream) => {
-                match process_request(stream) {
+                match process_request(stream, dir) {
                     Ok(_) => (),
                     Err(e) => println!("error while processing: {}", e),
                 };
@@ -27,7 +38,7 @@ fn main() {
     }
 }
 
-fn process_request(mut stream: TcpStream) -> Result<(), Error> {
+fn process_request(mut stream: TcpStream, directory: Option<String>) -> Result<(), Error> {
     let mut lines = BufReader::new(&stream).lines();
     let request_line = lines.next().ok_or(Error::other("empty stream"))??;
 
@@ -36,29 +47,24 @@ fn process_request(mut stream: TcpStream) -> Result<(), Error> {
     let _verb = parts.next().ok_or(Error::other("no HTTP verb"))?;
     let endpoint = parts.next().ok_or(Error::other("no endpoint"))?;
 
-    let ok = "HTTP/1.1 200 OK";
-    let crlf = "\r\n";
-    let not_found = "HTTP/1.1 404 Not Found".to_owned() + crlf + crlf;
-
-    let mut response: String = String::new();
-
     match endpoint {
-        "/" => response = format!("{ok}{crlf}{crlf}"),
+        "/" => return stream.write_all([OK, CRLF, CRLF].concat().as_slice()),
 
         s if s.starts_with("/echo/") => {
             let mut parts = s.split("/echo/");
             parts.next(); // skipping the empty
-
             if let Some(pong) = parts.next() {
-                if pong.is_empty() || pong.contains('/') {
-                    response = not_found.to_owned();
-                } else {
-                    let pong_len = pong.len();
-                    response = format!("{ok}{crlf}Content-Type: text/plain{crlf}Content-Length: {pong_len}{crlf}{crlf}{pong}");
-                }
-            } else {
-                response = not_found.to_owned();
+                return serve_echo(pong, stream);
             }
+        }
+
+        s if s.starts_with("/files/") => {
+            let mut parts = s.split("/files/");
+            parts.next(); // skipping the empty
+            if let Some(filename) = parts.next() {
+                return serve_file(directory, filename, stream);
+            }
+            return stream.write_all(NOT_FOUND);
         }
 
         "/user-agent" => {
@@ -66,21 +72,104 @@ fn process_request(mut stream: TcpStream) -> Result<(), Error> {
                 .map(|line| line.unwrap())
                 .take_while(|line| !line.is_empty())
             {
-                if line.starts_with("User-Agent: ") {
-                    let mut parts = line.split("User-Agent: ");
-                    parts.next();
-                    let agent = parts.next().unwrap().trim();
-                    let agent_len = agent.len();
-                    response = format!("{ok}{crlf}Content-Type: text/plain{crlf}Content-Length: {agent_len}{crlf}{crlf}{agent}");
+                if !line.starts_with("User-Agent: ") {
+                    continue;
                 }
+                let mut parts = line.split("User-Agent: ");
+                parts.next();
+                let agent = parts.next().unwrap().trim().as_bytes();
+                let agent_len = agent.len().to_string();
+                return stream.write_all(
+                    [
+                        OK,
+                        CRLF,
+                        b"Content-Type: text/plain",
+                        CRLF,
+                        b"Content-Length: ",
+                        agent_len.as_bytes(),
+                        CRLF,
+                        CRLF,
+                        agent,
+                    ]
+                    .concat()
+                    .as_slice(),
+                );
             }
-            if response.is_empty() {
-                response = not_found.to_owned();
-            }
+
+            // Header not found
+            return stream.write_all(NOT_FOUND);
         }
 
-        _ => response = not_found.to_owned(),
+        _ => return stream.write_all(NOT_FOUND),
     };
 
-    stream.write_all(response.as_bytes())
+    stream.write_all(NOT_FOUND)
+}
+
+fn serve_file(
+    directory: Option<String>,
+    filename: &str,
+    mut stream: TcpStream,
+) -> Result<(), Error> {
+    if filename.is_empty() || filename.contains('/') || filename.contains("..") {
+        return stream.write_all(NOT_FOUND);
+    }
+
+    let dir = directory.unwrap_or(String::from("."));
+
+    let mut file = match std::fs::File::open(format!("{dir}/{filename}")) {
+        Ok(file) => file,
+        Err(e) => {
+            stream.write_all(NOT_FOUND);
+            return Err(e);
+        }
+    };
+
+    let mut content: Vec<u8> = vec![];
+    match file.read_to_end(&mut content) {
+        Ok(bytes_read) => {
+            return stream.write_all(
+                [
+                    OK,
+                    CRLF,
+                    b"Content-Type: application/octet-stream",
+                    CRLF,
+                    b"Content-Length: ",
+                    bytes_read.to_string().as_bytes(),
+                    CRLF,
+                    CRLF,
+                    content.as_slice(),
+                ]
+                .concat()
+                .as_slice(),
+            );
+        }
+        Err(e) => {
+            stream.write_all(NOT_FOUND);
+            Err(e)
+        }
+    }
+}
+
+fn serve_echo(echo: &str, mut stream: TcpStream) -> Result<(), Error> {
+    if echo.is_empty() || echo.contains('/') {
+        return stream.write_all(NOT_FOUND);
+    }
+
+    let pong_len = echo.len().to_string();
+    return stream.write_all(
+        [
+            OK,
+            CRLF,
+            b"Content-Type: text/plain",
+            CRLF,
+            b"Content-Length: ",
+            pong_len.as_bytes(),
+            CRLF,
+            CRLF,
+            echo.as_bytes(),
+        ]
+        .concat()
+        .as_slice(),
+    );
 }
